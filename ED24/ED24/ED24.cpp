58253143a1af6902165fb0485e245f9e4d55d34c
Miguel Jesús Peñalver <lotiqui123@hotmail.com>
2020-03-23 23:30:15 +0100

    Exercise 24, correct

--- ED24/ED24/ED24.cpp
+++ ED24/ED24/ED24.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <sstream>
+#include <queue>
 
 #include "bintree_eda.h"
 
-bintree<long int> treeGenerator(std::vector<long int> const& preorder, std::vector<long int> const& inorder) {
+/*
+Miguel Jesús Peñalver Carvajal
+
+En este problema, dado el recorrido en preorden y en inorden de un árbol binario, devolvemos el recorrido 
+en postorden. Para la implementación he utilizado colas.
+El coste de este algoritmo es lineal resepecto al número de nodos.
+*/
+
+bintree<long int> treeGenerator(std::queue<long int> & preorder, std::queue<long int> & inorder) {
+	if (preorder.size() == 1)
+		return { preorder.front() };
 	
+	long int first = preorder.front();
+	std::queue<long int> preorderIzq, inorderIzq;
+	preorder.pop();
+
+	// Dividimos las colas hasta la raíz en preorden (el primer elemento) para generar los nodos
+	// del hijo izquierdo y los del hijo derecho.
+	while (inorder.front() != first) {
+		preorderIzq.push(preorder.front());
+		inorderIzq.push(inorder.front());
+		preorder.pop();
+		inorder.pop();
+	}
+	inorder.pop();
+	bintree<long int> left = {}, right = {};
+	
+	if (preorderIzq.size()) left = treeGenerator(preorderIzq, inorderIzq);
+	if (preorder.size()) right = treeGenerator(preorder, inorder);
+	
+	return { left, first, right };
 }
 
-std::vector<long int> read() {
-	std::vector<long int> ret;
+
+std::queue<long int> read() {
+	std::queue<long int> ret;
 	int tmp;
 	std::string s;
 
@@ -18,19 +48,20 @@ std::vector<long int> read() {
 	std::stringstream str(s);
 	
 	while (str >> tmp)
-		ret.push_back(tmp);
+		ret.push(tmp);
 	
 	return ret;
 }
 
 bool resuelveCaso() {
 	bintree<long int> t;
-	std::vector<long int> preorder, inorder;
+	std::queue<long int> preorder, inorder;
 
 	preorder = read();
+	if (preorder.size() == 0) return false;
 	inorder = read();
 
-	t = generateTree(preorder, inerder);
+	t = treeGenerator(preorder, inorder);
 
 	for (auto& i : t.postorder()) std::cout << i << ' ';
 	std::cout << '\n';
