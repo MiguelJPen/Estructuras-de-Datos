209094cfffc1533c46a898a8a28470db385b88e9
Miguel Jesús Peñalver <lotiqui123@hotmail.com>
2020-04-06 16:26:30 +0200

    86 v2

--- ED86/ED86/ED86.cpp
+++ ED86/ED86/ED86.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <algorithm>
+#include <vector>
 
 #include "bintree_eda.h"
 
@@ -8,7 +9,8 @@
 Miguel Jesús Peñalver Carvajal
 
 En este problema recibimos un árbol binario y devolvemos su perfil izquierdo.
-El coste de este algoritmo es O(n log(n)) por el método reverse().
+v1 - El coste de este algoritmo es O(n).
+v2 - El coste de este algoritmo es O(n).
 */
 
 typedef struct {
@@ -16,10 +18,35 @@ typedef struct {
 	std::list<int> v;
 }tSide;
 
-tSide leftSide(bintree<int> t) {
+std::vector<std::pair<int, int>> leftSidev2(bintree<int> t) {
+	std::vector<std::pair<int, int>> lefts;
+	int height = 0;
+
+	if (!t.empty()) {
+		std::queue<std::pair<bintree<int>, int>> pendientes;
+		pendientes.push(std::make_pair(t, height));
+		lefts.push_back(std::make_pair(t.root(), height));
+
+		while (!pendientes.empty()) {
+			std::pair<bintree<int>, int> sig = pendientes.front();
+			pendientes.pop();
+			if (lefts.back().second < sig.second)
+				lefts.push_back(std::make_pair(sig.first.root(), sig.second));
+
+			++sig.second;
+			if (!sig.first.left().empty())
+				pendientes.push(std::make_pair(sig.first.left(), sig.second));
+			if (!sig.first.right().empty())
+				pendientes.push(std::make_pair(sig.first.right(), sig.second));
+		}
+	}
+	return lefts;
+}
+
+tSide leftSidev1(bintree<int> t) {
 	if (t.empty()) return { 0, {} };
 	
-	tSide left = leftSide(t.left()), right = leftSide(t.right());
+	tSide left = leftSidev1(t.left()), right = leftSidev1(t.right());
 
 	if (right.height >= left.height)
 		right.v.resize(right.height - left.height);
@@ -38,11 +65,15 @@ void resuelveCaso() {
 	bintree<int> t;
 	t = leerArbol(-1);
 
-	std::list<int> l(leftSide(t).v);
-	l.reverse();
+	/*std::list<int> l(leftSidev1(t).v);
+	l.reverse();*/
+	std::vector<std::pair<int, int>> l = leftSidev2(t);
 	
+	/*for (auto const& x : l)
+		std::cout << x << ' ';*/
 	for (auto const& x : l)
-		std::cout << x << ' ';
+		std::cout << x.first << ' ';
+
 	std::cout << '\n';
 }
 
